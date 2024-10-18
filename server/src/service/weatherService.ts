e9f7bee58f91e4c90d0c9c538e86cd58cfa1c501
Maher Aboulhosn <maherhosn@gmail.com>
2024-10-18 01:13:09 -0400

    WeatherService.ts update with code + psedocode initialization

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,18 +2,88 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  name: string;
+  lat: string;
+  lon: string;
+  country: string;
+  state: string;
+}
+
+// const date = new Date(172930680000 * 10);
+// // British English uses day-month-year order and 24-hour time without AM/PM
+// console.log(date.toDateString());
 
 // TODO: Define a class for the Weather object
+class Weather {
+  // city
+  // today's date
+  // 5 day date forecast after today's date
+  // icon of the weather forecast
+  // Temprature for each day
+  // Wind speed for each day
+  // Humidity for each day
+  city: string;
+  date: number;
+  icon: string;
+  temprature: number;
+  wind: number;
+  humidity: number;
+
+  constructor(city: string, date: number, icon: string, temprature: number, wind: number, humidity: number) {
+    this.city = city;
+    this.date = date;
+    this.icon = icon;
+    this.temprature = temprature;
+    this.wind = wind;
+    this.humidity = humidity;
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL?: string;
+
+  private apiKey?: string;
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+
+    this.apiKey = process.env.API_KEY || '';
+  }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  async fetchLocationData(city: string) {
+    try {
+      const response = await fetch(
+        `${this.baseURL}/1.0/direct?q=${city}&api_key=${this.apiKey}`
+      );
+
+      const locationDetails = await response.json();
+
+      const mappedLocation = await this.destructureLocationData(locationDetails);
+      return mappedLocation;
+    } catch (err) {
+      console.log('Error:', err);
+      return err;
+    }
+  }
+
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(location: Coordinates): Coordinates {
+    const locationObject: Coordinates = {
+      name: location.name,
+      lat: location.lat,
+      lon: location.lon,
+      country: location.country,
+      state: location.state
+    };
+    return locationObject;
+  }
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string { 
+
+  }
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
